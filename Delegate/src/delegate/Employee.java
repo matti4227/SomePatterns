6cad33c7261c1a5dcfa95ae8923a20fd3e8f826d
matti <matti288422@gmail.com>
2018-10-31 21:02:16 +0100

    Delegate with Observer

--- Delegate/src/delegate/Employee.java
+++ Delegate/src/delegate/Employee.java
@@ -2,8 +2,20 @@ package delegate;
 
 public class Employee implements Worker {
 
+    private String name;
+    private Work work;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public void setWork(Work work) {
+        this.work = work;
+    }
+
     @Override
-    public void doWork() {
-        System.out.println(this.getClass().getName() + " pracuje... ");
+    public void doWork(Work whatWork) {
+        if(work == whatWork)
+            System.out.println(name + " pracuje... " + whatWork.getWork());
     }
 }
--- Delegate/src/delegate/Work.java
+++ Delegate/src/delegate/Work.java
@@ -1,5 +1,24 @@
 package delegate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Work {
 
+    protected List<Worker> workers = new ArrayList<>();
+
+    public abstract String getWork();
+
+    public void addWorker(Worker worker) {
+        workers.add(worker);
+    }
+
+    void removeWorker(Worker worker){
+        workers.remove(worker);
+    }
+
+//    void notifyWorkers(){
+//        for(Worker w : workers)
+//            w.doWork();
+//    }
 }
